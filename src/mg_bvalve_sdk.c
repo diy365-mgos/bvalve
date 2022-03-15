e1672b213d5d98485f16f21f5f36ba301dbdbbf2
fbalestrini <gaojing@hotmail.it>
2022-03-15 21:21:08 +0100

    Updated

--- src/mg_bvalve_sdk.c
+++ src/mg_bvalve_sdk.c
@@ -20,19 +20,39 @@ struct mg_bthing_actu *MG_BVALVE_CAST2(mgos_bvalve_t thing) {
 bool mg_bvalve_init(mgos_bvalve_t valve, enum mgos_bvalve_type valve_type,
                     struct mg_bvalve_cfg *valve_cfg,
                     struct mg_bsensor_cfg *sens_cfg) {
-  if (valve_cfg) {
-    // init actuator-base obj
-    if (mg_bactuator_init(valve, &valve_cfg->base, sens_cfg)) {
-      /* initalize general settings */
-      valve_cfg->valve_type = valve_type;
-      /* initalize overrides cfg */
-      valve_cfg->overrides.setting_state_cb = NULL;
-     
-      return true; // initialization successfully completed
+  bool is_type_ok = false;
+  if (((valve_type & MGOS_BVALVE_TYPE_SOLENOID) == MGOS_BVALVE_TYPE_SOLENOID) ||
+      ((valve_type & MGOS_BVALVE_TYPE_MOTORIZED) == MGOS_BVALVE_TYPE_MOTORIZED)) {
+    bool is_no = ((valve_type & MGOS_BVALVE_TYPE_NO) == MGOS_BVALVE_TYPE_NO);
+    bool is_nc = ((valve_type & MGOS_BVALVE_TYPE_NC) == MGOS_BVALVE_TYPE_NC);
+    if (is_no && is_nc) {
+      LOG(LL_ERROR, ("Invalid 'valve_type': 'NO' and 'NC' cannot both be active at the same time.", valve_type));
+    } else if (!is_no && !is_nc) {
+      LOG(LL_ERROR, ("Invalid 'valve_type': 'NO' or 'NC' must be set.", valve_type));
+    } else {
+      is_type_ok = true;
     }
-    mg_bvalve_reset(valve);
+  } else if ((valve_type & MGOS_BVALVE_TYPE_BISTABLE) == MGOS_BVALVE_TYPE_BISTABLE) {
+    is_type_ok = true;
+  }
+
+  if (is_type_ok) {
+    if (valve_cfg) {
+      // init actuator-base obj
+      if (mg_bactuator_init(valve, &valve_cfg->base, sens_cfg)) {
+        /* initalize general settings */
+        valve_cfg->valve_type = valve_type;
+        /* initalize overrides cfg */
+        valve_cfg->overrides.setting_state_cb = NULL;
+      
+        return true; // initialization successfully completed
+      }
+      mg_bvalve_reset(valve);
+    } else {
+      LOG(LL_ERROR, ("Invalid NULL 'valve_cfg' parameter."));
+    }
   } else {
-    LOG(LL_ERROR, ("Invalid NULL 'valve_cfg' parameter."));
+    LOG(LL_ERROR, ("Invalid 'valve_type' parameter value (%d).", valve_type));
   }
 
   LOG(LL_ERROR, ("Error initializing bValve '%s'. See above error message for more details.",
